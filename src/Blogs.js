32d82fbf73301c52d416527e659e5a33dce7863b
Steven <svramirez20@gmail.com>
2021-09-27 07:03:35 -0400

    pseudocode

--- src/Blogs.js
+++ src/Blogs.js
@@ -1,4 +1,4 @@
-import React from "react";
+//unused
 
 function BlogInfo({ blogs, setBlogs, deleteHandler, retrieveHandler }) {
 
--- src/Posts.js
+++ src/Posts.js
@@ -1,5 +1,5 @@
-import React from "react";
 import { useEffect } from "react";
+//unused
 
 function Posts() {
     const postPage = "http://jsonplaceholder.typicode.com/posts";
