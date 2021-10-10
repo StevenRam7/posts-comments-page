38ff4b7d41c2ceb0150546cbd20ff686d8008f70
Steven <svramirez20@gmail.com>
2021-10-09 23:38:33 -0400

    added time, buttons not working

--- src/App.js
+++ src/App.js
@@ -2,44 +2,40 @@ import './App.css';
 import { useState, useEffect } from "react";
 
 function App() {
-
   const [posts, setPosts] = useState([]);
   const [comments, setComments] = useState([]);
   const [buttonText, setButtonText] = useState("Show Comments");
   const [postID, setPostID] = useState("");
+  const [currentButtonText, setCurrentButtonText] = useState("");
+
+  const time = Date.now();
 
   const commentHandler = (event) => {
     event.preventDefault();
-    
+     
     if (buttonText === "Show Comments") {
       setButtonText("Hide Comments");
-      
-    }
+      }
     if (buttonText === "Hide Comments") {
       setButtonText("Show Comments")
-      
-    }
+      }
   }
+
 
   const postPage = "http://jsonplaceholder.typicode.com/posts";
   const commentPage = "http://jsonplaceholder.typicode.com/comments";
     
  useEffect (() => {
-   const abortController = new AbortController();
+   
    async function loadPosts() {
-     try{
-      const response = await fetch(postPage, { signal: abortController.signal });
+     
+      const response = await fetch(postPage);
       const postsFromAPI = await response.json();
       setPosts(postsFromAPI);
-     } catch(error) {
-       return <p>Page could not load!</p>
-     }
-      
-    }
+     } 
+          
      loadPosts();
-     return () => {
-       abortController.abort();
-     }     
+     
  }, []);
 
 useEffect (() => {
@@ -49,8 +45,8 @@ useEffect (() => {
      setComments(commentsFromAPI);
    }
     loadComments();     
-}, []);
-
+}, [postID]);
+//crashes when button is clicked for the first time
   return (
     <div className="App">
       <div className="header">
@@ -60,8 +56,15 @@ useEffect (() => {
       {posts.map((post) => (
           <div className="post-and-coms">
           <h3>{post.title.toUpperCase()}</h3><p>{"Post ID " + "#" + post.id}</p><p>{post.body}</p>
-          <button type="button" class="btn btn-primary btn-outline-dark" onClick={() => setPostID(post.id), commentHandler}>{buttonText}</button>
-          {buttonText === "Hide Comments" && comments.filter(comment => comment.postId === post.id).map((comment) => (
+          <button type="button" class="btn btn-primary btn-outline-dark" onClick={() => 
+            !postID ? setPostID(post.id) : setPostID("")             
+          }>{post.id === postID ? (
+            setCurrentButtonText("Hide Comments"),
+            currentButtonText
+          ) : 
+            buttonText 
+          }</button>
+          {currentButtonText === "Hide Comments" && comments.filter(comment => comment.postId === post.id).map((comment) => (
             <div className="comment">
             <h4>{comment.name}</h4>
             <p>{comment.body}</p>
@@ -73,7 +76,9 @@ useEffect (() => {
              
        )}
      </div>
-      
+      <div className="retrieval">
+        <p>{"Retrieved at: " + new Date(time).toLocaleString('en-US')}</p>
+      </div>
     </div>
   );
 }
